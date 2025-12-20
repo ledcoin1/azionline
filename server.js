07483783847fd720a9254f32f932cd97a072e6ae
ledcoin1 <ashamosugan@gmail.com>
2025-12-21 04:22:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
-const express = require("express");
-const http = require("http");
 const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static("public"));
-
-let waiting = null;
+const io = new Server(3000);
 
 io.on("connection", socket => {
   console.log("Клиент қосылды");
 
+  // Клиент хабар жібергенде орындалады
   socket.on("joinGame", ({ userId }) => {
-    if (!waiting) {
-      waiting = { socket, userId };
-      socket.emit("message", "Сіз бірінші ойыншы, қарсылас күтілуде...");
-    } else {
-      const other = waiting;
-      socket.emit("message", "Қарсылас табылды! Ойын басталды.");
-      other.socket.emit("message", "Қарсылас қосылды! Ойын басталды.");
-      waiting = null;
-    }
+    console.log("Ойынға қосылды:", userId);
+
+    // Клиентке хабар жібереміз
+    socket.emit("message", "Сіз ойынға қосылдыңыз!");
   });
 });
-
-server.listen(3000, () => console.log("Server running on port 3000"));
