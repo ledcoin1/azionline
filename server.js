adfcd060f988c4627352e8aaae815a307e610238
ledcoin1 <ashamosugan@gmail.com>
2025-12-21 05:16:52 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+// Қажетті кітапханаларды шақырамыз
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+// Серверді жасаймыз
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Клиентке статикалық файлдарды береміз
+app.use(express.static("public"));
+
+// Клиент қосылғанда орындалатын код
+io.on("connection", (socket) => {
+  console.log("Клиент қосылды");
+
+  // Клиенттен хабар келгенде орындалады
+  socket.on("joinGame", ({ userId }) => {
+    console.log("Ойынға қосылды:", userId);
+
+    // Сервер хабарын клиентке қайта жіберу
+    socket.emit("message", `Сәлем, ойыншы ${userId}! Сіз серверге қосылдыңыз.`);
+  });
+});
+
+// Серверді 3000 портында іске қосамыз
+server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
 
