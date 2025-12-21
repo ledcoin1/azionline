5a837aadc3fe74ff62ca8c9bf0795d2a327b769b
ledcoin1 <ashamosugan@gmail.com>
2025-12-21 06:00:22 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-// express + http + socket.io
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
-// express қосамыз
 const app = express();
 const server = http.createServer(app);
-
-// socket.io орнатамыз
 const io = new Server(server);
 
-// статикалық файлдар (index.html) үшін
 app.use(express.static("public"));
+
+// Бірінші ойыншыны сақтау
+let waitingPlayer = null;
 
 io.on("connection", (socket) => {
   console.log("Клиент қосылды");
 
-  // Клиент хабар жібергенде орындалады
   socket.on("joinGame", ({ userId }) => {
     console.log("Ойынға қосылды:", userId);
 
-    // Сервер хабарын клиентке қайта жіберу
-    socket.emit("message", `Сәлем, ойыншы ${userId}! Сіз серверге қосылдыңыз.`);
+    if (!waitingPlayer) {
+      // Бірінші ойыншы күтілуде
+      waitingPlayer = { socket, userId };
+      socket.emit("message", "Сіз бірінші ойыншы, қарсылас күтілуде...");
+    } else {
+      // Екінші ойыншы қосылды → екі жаққа хабар беру
+      const first = waitingPlayer;
+      waitingPlayer = null;
+
+      first.socket.emit("message", `Қарсылас табылды! Екінші ойыншы: ${userId}`);
+      socket.emit("message", `Қарсылас табылды! Бірінші ойыншы: ${first.userId}`);
+    }
   });
 });
 
-
-// серверді порт 3000-де іске қосамыз
 server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
 
