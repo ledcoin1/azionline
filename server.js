3a20509c28822a18c778b6c9c26379f42bcc7c00
ledcoin1 <ashamosugan@gmail.com>
2026-01-14 23:04:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,22 @@ let players={};
 //miniapp ashkanda
 io.on("connection",socket=>{
   console.log("ал кірді: ",socket.id);
+
+  socket.on("playerJoined",players=>{
+    players[socket.id]=players;
+
+    console.log("осындай ойыншы кірді ",player);
+    console.log("қазіргі ойыншылар "players);
+  })
 })
+
+
 
 
 http.listen(PORT, () => {
     console.log(`Server ${PORT} портында жұмыс істеп тұр`);
 });
+
 
 
 
