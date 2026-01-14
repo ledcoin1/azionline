3e6ad9f48cab258f6da3afe5c81420507b57a4db
ledcoin1 <ashamosugan@gmail.com>
2026-01-14 20:37:59 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const io = require("socket.io")(3000);
+// Порт автоматты түрде беріледі немесе 3000
+const PORT = process.env.PORT || 3000;
+const io = require("socket.io")(PORT);
+
+let players = {}; // Қосылған ойыншылар
 
 io.on("connection", socket => {
     console.log("Жаңа ойыншы қосылды:", socket.id);
 
-    // Telegram Mini App арқылы кім қосылғанын көру
+    // Клиент қосылған кезде Telegram Mini App арқылы хабар
     socket.on("playerJoined", player => {
+        players[socket.id] = player;
         console.log("Ойыншы Telegram арқылы кірді:", player);
+        console.log("Қазіргі ойыншылар:", players);
     });
 
     socket.on("disconnect", () => {
+        delete players[socket.id];
         console.log("Ойыншы шықты:", socket.id);
+        console.log("Қалған ойыншылар:", players);
     });
 });
+
+console.log(`Socket.IO сервер ${PORT} портында жұмыс істеп тұр`);
