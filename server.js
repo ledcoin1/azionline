4c7f844729bf72f9edff03e0aeda45e7371080e3
ledcoin1 <ashamosugan@gmail.com>
2026-01-20 20:42:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,8 @@ io.on("connection", socket => {
 });
 
 
-
+socket.on("play_click", (data) => {
+    console.log("Играть батырмасы басылды:", data);
 
 
 
@@ -131,6 +132,7 @@ io.on("connection", socket => {
 http.listen(PORT, () => {
   console.log(`Server ${PORT} портында жұмыс істеп тұр`);
 });
+
 
 
 
