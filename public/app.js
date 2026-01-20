8f38d6afd54b1c08fa80121ef0f385a9d8fbd038
ledcoin1 <ashamosugan@gmail.com>
2026-01-20 20:33:31 +0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,25 @@
 
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+// Telegram user
+const user = tg.initDataUnsafe.user;
+
+if(!user){
+  alert("Telegram арқылы кір!");
+}
+
+const telegramId = user.id;
+
+uid.innerText = telegramId;
+
+// backend login
+fetch("/api/login",{
+  method:"POST",
+  headers:{ "Content-Type":"application/json" },
+  body:JSON.stringify({ telegramId })
+})
+.then(r=>r.json())
+.then(data=>{
+  bal.innerText = data.balance;
+});
