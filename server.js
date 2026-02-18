9d2bc908dcfecd72dc040c0c1c0974614e61d9af
ledcoin1 <ashamosugan@gmail.com>
2026-02-18 20:58:44 +0600

    Refactor server.js for better structure and clarity

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ io.on("connection",(socket)=>{
 
 
 
-
+console.log(komta);
 
 
 
@@ -102,6 +102,7 @@ io.on("connection",(socket)=>{
 http.listen(PORT, () => {
   console.log(`Server ${PORT} портында жұмыс істеп тұр`);
 });
+
 
 
 
